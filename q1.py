3912b2be3ac38ee7a8c8457d051fd4ab20b9832d
dimii27 <dimitrieietcu@gmail.com>
2020-07-22 15:55:04 +0300

    q1 done

--- q1.py
+++ q1.py
@@ -3,12 +3,11 @@ class Person(object):
         self.name = name
     last_word = "word"
     def say(self, stuff):
-        last_word = stuff
+        self.last_word = stuff
         return stuff
     def ask(self, stuff):
         return self.say("Would you please " + stuff)
     def greet(self):
         return self.say("Hello, my name is " + self.name)
     def repeat(self):
-        last = Person.last_word
-        return last
+        return self.last_word
